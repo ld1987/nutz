373c384d56cbf265700eaf048f424cd949fd6e91
Administrator <Administrator@PC201401022044>
2014-01-06 22:25:45 +0800

    测试

--- src/org/nutz/package-info.java
+++ src/org/nutz/package-info.java
@@ -2,7 +2,7 @@
  * 对于 Java 程序员来说，除 SSH 之外，的另一个选择:
  * <p>
  * <b>Nutz</b>
- * <p>
+ * <p>12312321321
  * 当然，它是开源的，并且是完全免费的,同时也是商业友好的(Licensed under the Apache License, Version 2.0)。
  */
 package org.nutz;
